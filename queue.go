d0401c8521efda5f73814b4780dfc0607f7ed122
canta2899 <andreacantarutti@icloud.com>
2022-02-22 13:37:16 +0100

    Adding queue implementation

---
+++ queue.go
@@ -0,0 +1,30 @@
+package main
+
+import "errors"
+
+type Queue struct {
+	maxc  int
+	queue chan interface{}
+}
+
+func (q *Queue) Enqueue(entry interface{}) error {
+	if len(q.queue) >= q.maxc {
+		return errors.New("Queue is full")
+	}
+
+	q.queue <- entry
+	return nil
+}
+
+func (q *Queue) Dequeue() (interface{}, error) {
+	if len(q.queue) == 0 {
+		return nil, errors.New("Queue is empty")
+	}
+
+	next := <-q.queue
+	return next, nil
+}
+
+func NewQueue(maxc int) *Queue {
+	return &Queue{maxc, make(chan interface{}, maxc)}
+}

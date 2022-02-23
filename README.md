1f2d3055bd4c6e6f9c17ad548fb2764a3e095ae2
canta2899 <andreacantarutti@icloud.com>
2022-02-23 04:11:10 +0100

    Updating readme

--- README.md
+++ README.md
@@ -9,3 +9,23 @@ grep -r -l "mypattern" /my/starting/path
 ```
 
 withouth requiring **17** years to complete.
+
+## Installation
+
+Clone this repository and compile the `main.go` by running
+
+```sh
+
+go build . -o mgrep
+
+```
+
+Then, put the executable on your path or symlink it as follows
+
+```sh
+
+ln -s mgrep /usr/local/bin/mgrep
+
+```
+
+If everything works, running `mgrep` should prompt you a brief usage guide.
--- main.go
+++ main.go
@@ -4,12 +4,12 @@
     of file content using regexps and goroutines
 
     Usage
-    =====
+    ======
 
     mgrep [pattern] [startpath]
 
     Flags
-    =====
+    ======
         • -w  (degree of parallelism [default 8])
         • -x  (directories to be excluded)
 
@@ -23,141 +23,141 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"os"
-	"path"
-	"path/filepath"
-	"regexp"
-	"sync"
+    "flag"
+    "fmt"
+    "os"
+    "path"
+    "path/filepath"
+    "regexp"
+    "sync"
 )
 
 type ExcludedDirs []string
 
 func (ed *ExcludedDirs) String() string {
-	return fmt.Sprintln(*ed)
+    return fmt.Sprintln(*ed)
 }
 
 func (ed *ExcludedDirs) Set(s string) error {
-	fullpath, err := filepath.Abs(s)
-	if err != nil {
-		return err
-	}
-	*ed = append(*ed, fullpath)
-	return nil
+    fullpath, err := filepath.Abs(s)
+    if err != nil {
+        return err
+    }
+    *ed = append(*ed, fullpath)
+    return nil
 }
 
 type Parameters struct {
-	startpath string
-	pattern   string
-	workers   int
-	excluded  ExcludedDirs
+    startpath string
+    pattern   string
+    workers   int
+    excluded  ExcludedDirs
 }
 
 func ParseArgs() *Parameters {
-	var exc ExcludedDirs
-	w := flag.Int("w", 8, "degree of parallelism")
-	flag.Var(&exc, "x", "excluded paths")
+    var exc ExcludedDirs
+    w := flag.Int("w", 8, "degree of parallelism")
+    flag.Var(&exc, "x", "excluded paths")
 
-	flag.Parse()
+    flag.Parse()
 
-	pattern := flag.Arg(0)
-	startpath := flag.Arg(1)
+    pattern := flag.Arg(0)
+    startpath := flag.Arg(1)
 
-	return &Parameters{workers: *w, excluded: exc, startpath: startpath, pattern: pattern}
+    return &Parameters{workers: *w, excluded: exc, startpath: startpath, pattern: pattern}
 }
 
 func handler(c <-chan interface{}, wg *sync.WaitGroup, r *regexp.Regexp) {
-	defer wg.Done()
+    defer wg.Done()
 
-	for {
+    for {
 
-		if len(c) == 0 {
-			return
-		}
+        if len(c) == 0 {
+            return
+        }
 
-		next := <-c
+        next := <-c
 
-		filepath := next.(string) // type assertion
+        filepath := next.(string) // type assertion
 
-		fi, err := os.Stat(filepath)
+        fi, err := os.Stat(filepath)
 
-		if err != nil || !fi.Mode().IsRegular() {
-			// Skips
-			continue
-		}
+        if err != nil || !fi.Mode().IsRegular() {
+            // Skips
+            continue
+        }
 
-		filedata, err := os.ReadFile(filepath)
+        filedata, err := os.ReadFile(filepath)
 
-		if err != nil {
-			// Skips
-			continue
-		}
+        if err != nil {
+            // Skips
+            continue
+        }
 
-		if r.Match(filedata) {
-			fmt.Println(next)
-		}
-	}
+        if r.Match(filedata) {
+            fmt.Println(next)
+        }
+    }
 }
 
 func main() {
 
-	// Allows synchronization in order to let the
-	// main function wait for the other coroutines
-	var wg sync.WaitGroup
+    // Allows synchronization in order to let the
+    // main function wait for the other coroutines
+    var wg sync.WaitGroup
 
-	params := ParseArgs()
+    params := ParseArgs()
 
-	r, _ := regexp.Compile(params.pattern)
+    r, _ := regexp.Compile(params.pattern)
 
-	files := []string{}
+    files := []string{}
 
-	err := filepath.Walk(params.startpath,
-		func(pathname string, info os.FileInfo, err error) error {
+    err := filepath.Walk(params.startpath,
+        func(pathname string, info os.FileInfo, err error) error {
 
-			// Should exlude some paths if specified
-			if isin(path.Dir(pathname), params.excluded) {
-				return filepath.SkipDir
-			}
+            // Should exlude some paths if specified
+            if isin(path.Dir(pathname), params.excluded) {
+                return filepath.SkipDir
+            }
 
-			if err != nil {
-				return err
-			}
+            if err != nil {
+                return err
+            }
 
-			files = append(files, pathname)
+            files = append(files, pathname)
 
-			return nil
-		})
+            return nil
+        })
 
-	if err != nil {
-		panic(err.Error())
-	}
+    if err != nil {
+        panic(err.Error())
+    }
 
-	// Using a channel in order to implement a thread
-	// safe queue that allows multiple consumers to
-	// "dequeue" each path and process the content of
-	// the referred file
-	c := make(chan interface{}, len(files))
+    // Using a channel in order to implement a thread
+    // safe queue that allows multiple consumers to
+    // "dequeue" each path and process the content of
+    // the referred file
+    c := make(chan interface{}, len(files))
 
-	for _, p := range files {
-		c <- p
-	}
+    for _, p := range files {
+        c <- p
+    }
 
-	wg.Add(params.workers)
+    wg.Add(params.workers)
 
-	for i := 0; i < params.workers; i++ {
-		go handler(c, &wg, r)
-	}
+    for i := 0; i < params.workers; i++ {
+        go handler(c, &wg, r)
+    }
 
-	wg.Wait()
+    wg.Wait()
 }
 
 // Utility that checks whether el is in list
 func isin(el string, list ExcludedDirs) bool {
-	for _, entry := range list {
-		if el == entry {
-			return true
-		}
-	}
-	return false
+    for _, entry := range list {
+        if el == entry {
+            return true
+        }
+    }
+    return false
 }

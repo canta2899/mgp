dbdad2e57a01351a099c3850457e7135692fb4c6
canta2899 <andreacantarutti@icloud.com>
2022-04-28 01:41:07 +0200

    feat: Faster sigterm handler and minor enhancements

--- main.go
+++ main.go
@@ -11,20 +11,21 @@ import (
 	"regexp"
 	"sync"
 	"syscall"
+
 	"github.com/fatih/color"
 )
 
 type Entry struct {
-    Path string
-    Info *os.FileInfo
+	Path string
+	Info *os.FileInfo
 }
 
 type MessageType int64
 
 const (
-    Match MessageType = iota
-    ErrorMatch
-    Log
+	MatchMessage MessageType = iota
+	ErrorMatchMessage
+	TextMessage
 )
 
 var coloredOutput bool = true
@@ -34,173 +35,188 @@ const OK string = string('\u2713')
 const KO string = string('\u00D7')
 
 // Colors for printing
-var green =  color.New(color.FgHiGreen).SprintFunc()
-var red   =  color.New(color.FgRed).SprintFunc()
+var green = color.New(color.FgHiGreen).SprintFunc()
+var red = color.New(color.FgRed).SprintFunc()
 
 func printHandler(message string, messageType MessageType) {
-    switch messageType {
-    case Match:
-        if coloredOutput {
-            log.Printf("%v %v\n", green(OK), message)
-        } else {
-            log.Printf("%v\n", message)
-        }
-        return;
-    case ErrorMatch:
-        if coloredOutput {
-            log.Printf("%v %v\n", red(KO), message)
-        }
-        return;
-    }
+	switch messageType {
+	case MatchMessage:
+		if coloredOutput {
+			log.Printf("%v %v\n", green(OK), message)
+		} else {
+			log.Printf("%v\n", message)
+		}
+		return
+	case ErrorMatchMessage:
+		if coloredOutput {
+			log.Printf("%v %v\n", red(KO), message)
+		}
+		return
+	}
 
-    log.Printf(message)
+	log.Printf(message)
 }
 
-
 // Routine performed by each worker
-func handler(ch <-chan *Entry, wg *sync.WaitGroup, r *regexp.Regexp) {
-    defer wg.Done()
+func handler(ch <-chan *Entry, closech <-chan bool, wg *sync.WaitGroup, r *regexp.Regexp) {
+	defer wg.Done()
 
-    for {
+	for {
 
-        e, more := <-ch
+		select {
+		case <-closech:
+			break
+		case e, more := <-ch:
+			if !more {
+				return
+			}
 
-        if !more {
-            return
-        }
+			info, fullpath := e.Info, e.Path
 
-        info, fullpath := e.Info, e.Path
+			if !(*info).Mode().IsRegular() {
+				continue // Skips
+			}
 
-        if !(*info).Mode().IsRegular() {
-            continue // Skips
-        }
+			file, err := os.Open(fullpath)
 
-        file, err := os.Open(fullpath)
+			if err != nil {
+				continue // Skips
+			}
 
-        if err != nil {
-            continue // Skips
-        }
+			bufread := bufio.NewReader(file)
 
-        bufread := bufio.NewReader(file)
+			for {
+				line, err := bufread.ReadBytes('\n')
 
-        for {
-            line, err := bufread.ReadBytes('\n')
+				if err == io.EOF {
+					break
+				}
 
-            if err == io.EOF {
-                break
-            }
-
-            if r.Match(line) {
-                printHandler(fullpath, Match)
-                break
-            }
-        }
-        file.Close()
-    }
+				if r.Match(line) {
+					printHandler(fullpath, MatchMessage)
+					break
+				}
+			}
+			file.Close()
+		default:
+			continue
+		}
+	}
 }
 
 // Process path and enqueues if ok for match checking
 func processPath(info *os.FileInfo, pathname string, c chan *Entry, params *Parameters) error {
-    isdir := (*info).IsDir()
-    exc := params.GetExcludedDirs()
-    lim := params.limitMb
+	isdir := (*info).IsDir()
+	exc := params.GetExcludedDirs()
+	lim := params.limitMb
 
-    for _, n := range exc {
-        fullMatch, _ := filepath.Match(n, pathname)
-        baseMatch, _ := filepath.Match(n, filepath.Base(pathname))
-        if isdir && (fullMatch || baseMatch) {
-            return filepath.SkipDir 
-        }
-    }
+	for _, n := range exc {
+		fullMatch, _ := filepath.Match(n, pathname)
+		baseMatch, _ := filepath.Match(n, filepath.Base(pathname))
+		if isdir && (fullMatch || baseMatch) {
+			return filepath.SkipDir
+		}
+	}
 
-    if !isdir && (*info).Size() < int64(lim) {
-        c <- &Entry{Path: pathname, Info: info} 
-    }
+	if !isdir && (*info).Size() < int64(lim) {
+		c <- &Entry{Path: pathname, Info: info}
+	}
 
-    return nil
+	return nil
 }
 
 // Evaluates error for path and returns action to perform
 func handlePathError(info *os.FileInfo, pathname string, err error) error {
 
-    if os.IsNotExist(err) { log.Fatal("Invalid path") }
+	if os.IsNotExist(err) {
+		log.Fatal("Invalid path")
+	}
 
-    // Prints error line for current path
-    printHandler(pathname, ErrorMatch)
-    printHandler(err.Error(), Log)
-    
-    if (*info).IsDir() {
-        return filepath.SkipDir
-    } else {
-        return nil
-    }
+	// Prints error line for current path
+	printHandler(pathname, ErrorMatchMessage)
+	printHandler(err.Error(), TextMessage)
+
+	if (*info).IsDir() {
+		return filepath.SkipDir
+	} else {
+		return nil
+	}
 }
 
 // Handler for sigterm (ctrl + c from cli)
-func setSignalHandlers(closed *bool, wg *sync.WaitGroup) {
-    sigch := make(chan os.Signal, 1)
-    signal.Notify(sigch, os.Interrupt, syscall.SIGTERM)
-    go func() {
-        <-sigch 
-        *closed = true
-    }()
-}
+func setSignalHandlers(closed []chan bool, stopWalk *bool, wg *sync.WaitGroup) {
+	sigch := make(chan os.Signal, 1)
+	signal.Notify(sigch, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-sigch
 
-func setupLogger() {
-    // Message wih no flags printed to stdout
-    log.SetFlags(0)
-    log.SetOutput(os.Stdout)
+		*stopWalk = true
+
+		for _, closeChan := range closed {
+			closeChan <- true
+		}
+
+	}()
 }
 
 func main() {
-    var wg sync.WaitGroup
-    ch := make(chan *Entry, 5000)
-    closed := false
 
-    setupLogger()
-    setSignalHandlers(&closed, &wg)
-    params := ParseArgs()
+	log.SetFlags(0)
+	log.SetOutput(os.Stdout)
 
-    pattern := params.pattern
-    coloredOutput = !params.nocolor
-    
-    if params.icase {
-        pattern = "(?i)" + pattern
-    }
+	params := ParseArgs()
 
-    r, _ := regexp.Compile(pattern)
+	coloredOutput = !params.nocolor
 
-    wg.Add(params.workers)
-    for i := 0; i < params.workers; i++ {
-        go handler(ch, &wg, r)
-    }
+	pattern := params.pattern
+	if params.icase {
+		pattern = "(?i)" + pattern
+	}
+	r, _ := regexp.Compile(pattern)
 
-    filepath.Walk(params.startpath,
+	var wg sync.WaitGroup
+	stopWalk := false
+	closeSignalChans := make([]chan bool, params.workers)
+	setSignalHandlers(closeSignalChans, &stopWalk, &wg)
 
-        func(pathname string, info os.FileInfo, err error) error {
+	ch := make(chan *Entry, 5000)
 
-            if closed {
-                // If the termination is requested, the path Walking
-                // stops and the function returns with an error
-                return errors.New("User requested termination")
-            }
+	wg.Add(params.workers)
+	for i := 0; i < params.workers; i++ {
+		closeChan := make(chan bool)
+		closeSignalChans = append(closeSignalChans, closeChan)
+		go handler(ch, closeChan, &wg, r)
+	}
 
-            // Checking permission and access errors
-            if err != nil {
-                return handlePathError(&info, pathname, err)
-            }
+	filepath.Walk(params.startpath,
 
-            // Processes path in search of matches with the given
-            // pattern or the excluded directories 
-            return processPath(&info, pathname, ch, params)
+		func(pathname string, info os.FileInfo, err error) error {
 
-        })
+			if stopWalk {
+				// If the termination is requested, the path Walking
+				// stops and the function returns with an error
+				return errors.New("User requested termination")
+			}
 
-    // The channel is closed, this communicates that
-    // no more values will be enqueued
-    close(ch)
+			// Checking permission and access errors
+			if err != nil {
+				return handlePathError(&info, pathname, err)
+			}
 
-    // Waits for goroutines to finish
-    wg.Wait()
+			// Processes path in search of matches with the given
+			// pattern or the excluded directories
+			return processPath(&info, pathname, ch, params)
+
+		})
+
+	// The channel is closed, this communicates that
+	// no more values will be enqueued
+	close(ch)
+
+	// Waits for goroutines to finish
+	wg.Wait()
+
+	if stopWalk {
+		printHandler("Ended by user", TextMessage)
+	}
 }
-
--- parser.go
+++ parser.go
@@ -14,60 +14,67 @@ const PROG_NAME = "mgp"
 var STD_EXC_DIRS = []string{".bzr", "CVS", ".git", ".hg", ".svn", ".idea", ".tox"}
 
 type Flags struct {
-    workers   int
-    nocolor   bool         
-    icase     bool
-    exclude   string
-    limitMb   int
+	workers int
+	nocolor bool
+	icase   bool
+	exclude string
+	limitMb int
 }
 
-func (f *Flags) GetExcludedDirs() []string{
-    if f.exclude == "" {
-        return STD_EXC_DIRS 
-    } 
+func (f *Flags) GetExcludedDirs() []string {
+	if f.exclude == "" {
+		return STD_EXC_DIRS
+	}
 
-    return append(STD_EXC_DIRS, strings.Split(f.exclude, ",")...)
+	return append(STD_EXC_DIRS, strings.Split(f.exclude, ",")...)
 }
 
 type Parameters struct {
-    Flags
-    startpath string
-    pattern   string
+	Flags
+	startpath string
+	pattern   string
 }
 
 func PrintVersionAndExit() {
-    log.Println(PROG_NAME, VERSION)
-    os.Exit(0)
+	log.Println(PROG_NAME, VERSION)
+	os.Exit(0)
 }
 
 func PrintBriefHelpAndExit() {
-    log.Println("Usage:", PROG_NAME, "[options] pattern starting/path")
-    log.Println("Run", PROG_NAME, "-h for more information")
-    os.Exit(0);
+	log.Println("Usage:", PROG_NAME, "[options] pattern starting/path")
+	log.Println("Run", PROG_NAME, "-h for more information")
+	os.Exit(0)
 }
 
 func ParseArgs() *Parameters {
 
-    f := Flags{}
+	f := Flags{}
 
-    flag.IntVar(&f.workers, "w", 16, "Defines the number of workers")
-    flag.IntVar(&f.limitMb, "l", 100, "File size limit")
-    flag.BoolVar(&f.icase, "i", false, "Performs case insensitive matching")
-    flag.BoolVar(&f.nocolor, "c", false, "Disable colored output")
-    flag.StringVar(&f.exclude, "e", "", "Excluded paths (specified as a comma separated list like \"path1,path2\")")
+	printVersion := false
 
-    f.limitMb = f.limitMb * MEGABYTE
+	flag.IntVar(&f.workers, "w", 16, "Defines the number of workers")
+	flag.BoolVar(&printVersion, "v", false, "Prints current mgp version")
+	flag.IntVar(&f.limitMb, "l", 100, "File size limit")
+	flag.BoolVar(&f.icase, "i", false, "Performs case insensitive matching")
+	flag.BoolVar(&f.nocolor, "c", false, "Disable colored output")
+	flag.StringVar(&f.exclude, "e", "", "Excluded paths (specified as a comma separated list like \"path1,path2\")")
 
-    flag.Parse()
+	f.limitMb = f.limitMb * MEGABYTE
 
-    posArgs := flag.Args()
-    if len(posArgs) < 2 {
-        PrintBriefHelpAndExit()
-    }
+	flag.Parse()
 
-    return &Parameters{
-        Flags: f,
-        pattern: posArgs[0],
-        startpath: posArgs[1],
-    }
+	posArgs := flag.Args()
+	if len(posArgs) < 2 {
+		PrintBriefHelpAndExit()
+	}
+
+	if printVersion {
+		PrintVersionAndExit()
+	}
+
+	return &Parameters{
+		Flags:     f,
+		pattern:   posArgs[0],
+		startpath: posArgs[1],
+	}
 }

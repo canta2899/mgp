d8068d52166561fe14671283e0beed68df733125
canta2899 <andreacantarutti@icloud.com>
2022-10-18 18:59:32 +0200

    feat: changing default goroutines limit

--- cli/parser.go
+++ cli/parser.go
@@ -64,7 +64,7 @@ func ParseArgs() *Parameters {
 
 	printVersion := false
 
-	flag.IntVar(&f.workers, "w", 100, "Defines the max number of routines running at the same time")
+	flag.IntVar(&f.workers, "w", 1000, "Defines the max number of routines running at the same time")
 	flag.BoolVar(&printVersion, "v", false, "Prints current mgp version")
 	flag.IntVar(&f.limitBytes, "lim", 100, "File size limit")
 	flag.BoolVar(&f.icase, "i", false, "Performs case insensitive matching")
